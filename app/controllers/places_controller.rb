db0d2309cd89767d95f9a8ce0ca4a1d4417402a3
iuval <iuval.goldansky@neonroots.com>
2013-10-24 23:53:13 -0200

    Adds place partial to lists places with 'Show in map' button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
 	def index
-    @markers = Place.all.map{ |place|
+    @places = Place.page(params[:page])
+    @markers = @places.map{ |place|
       [place.address.lat,
        place.address.long,
        place.name + ': ' + place.accessibilities.map(&:name).join(', ')]
