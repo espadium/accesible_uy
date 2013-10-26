ce8745f568c8d39787704f38eb3aa31ba56ef737
Oscar Siniscalchi <oscarsiniscalchi@gmail.com>
2013-10-26 08:44:46 -0200

    Subscription basic.

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,32 @@
+class SubscriptionsController < ApplicationController
+	def index
+		@accessibilities = Accessibility.all
+		@subscription = Subscription.new
+	end
+
+	def new
+	end
+
+	def create
+
+		@subscription = Subscription.new(subscription_params)
+		params[:accessibilities].each do |a|
+			@subscription.accessibilities << Accessibility.find(a[0])
+		end
+		if @subscription.valid?
+		  @subscription.save
+		  flash[:notice] = 'Felicitaciones te has subscripto. Recibiras informacion cuando este disponible.'
+		  redirect_to subscriptions_path
+		end
+	end
+
+	private
+    # Using a private method to encapsulate the permissible parameters is
+    # just a good pattern since you'll be able to reuse the same permit
+    # list between create and update. Also, you can specialize this method
+    # with per-user checking of permissible attributes.
+    def subscription_params
+      params.require(:subscription).permit(:subscriptor)
+    end
+
+end
