1bace52e99df923e480474eedbb6445817399d6f
iuval <iuval.goldansky@neonroots.com>
2013-12-04 22:48:15 -0200

    Add institution show

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -2,10 +2,26 @@ class InstitutionsController < ApplicationController
   def by_accessibility
     @accessibility = Accessibility.friendly.find(params[:id])
     @institutions = @accessibility.institutions
+    @markers = @institutions.all.map{ |institution|
+      [institution.address.lat,
+       institution.address.long,
+       institution.name,
+       institution.accessibilities.map(&:name)]
+    }
     render :index
+  end
+
+  def show
+    @institution = Institution.find(params[:id])
   end
 
   def index
     @institutions = Institution.all
+    @markers = @institutions.all.map{ |institution|
+      [institution.address.lat,
+       institution.address.long,
+       institution.name,
+       institution.accessibilities.map(&:name)]
+    }
   end
 end
