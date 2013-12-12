d641c84f9cda345371456e382c93009d17fdcc60
Gabriel Osorio <gabriel.osorio.montelius@gmail.com>
2013-12-11 23:27:37 -0200

    Fix undeclared pagination helper method.

--- app/helpers/pagination.rb
+++ app/helpers/pagination.rb
@@ -1,5 +1,9 @@
-if page.left_outer? || page.right_outer? || page.inside_window?
-  return page_tag page
-elsif !page.was_truncated?
-  return gap_tag
+module PaginationHelper
+  def tag_or_gap_for(page)
+    if page.left_outer? || page.right_outer? || page.inside_window?
+      return page_tag page
+    elsif !page.was_truncated?
+      return gap_tag
+    end
+  end
 end
